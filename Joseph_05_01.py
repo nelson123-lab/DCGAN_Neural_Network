4e8328a5dc2ffcebc21e7a43ef30c5f76fe7eb0e
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-04-25 23:15:33 -0500

    pushing

--- Joseph_05_01.py
+++ Joseph_05_01.py
@@ -1,3 +1,7 @@
+
+
+
+
 import os
 import tensorflow as tf
 import numpy as np
@@ -38,6 +42,29 @@ class GenerateSamplesCallback(tf.keras.callbacks.Callback):
         # close the plot to free up memory
         plt.close()
 
+# def build_discriminator():
+#     """
+#     The discriminator model takes an image input with a shape of (28, 28, 1) and outputs a single
+#     value that indicates the probability of the input image being real or fake.
+
+#     Model Architecture:
+#     1. Conv2D layer with 16 filters, kernel size of (5, 5), strides of (2, 2), and padding set to 'same'.
+#     2. LeakyReLU activation function (default parameters)
+#     3. Dropout layer with a rate of 0.3.
+#     4. Conv2D layer with 32 filters, kernel size of (5, 5), strides of (2, 2), and padding set to 'same'.
+#     5. LeakyReLU activation function (default parameters)
+#     6. Dropout layer with a rate of 0.3.
+#     7. Flatten layer to convert the feature maps into a 1D array.
+#     8. Dense layer with 1 output neuron.
+
+# Returns:
+#     model (tf.keras.models.Sequential): A TensorFlow Keras Sequential model representing the discriminator.
+#     :return:
+#     """
+#     model = tf.keras.models.Sequential()
+#     # your code here
+
+#     return model
 def build_discriminator():
     """
     The discriminator model takes an image input with a shape of (28, 28, 1) and outputs a single
@@ -53,42 +80,48 @@ def build_discriminator():
     7. Flatten layer to convert the feature maps into a 1D array.
     8. Dense layer with 1 output neuron.
 
-Returns:
-    model (tf.keras.models.Sequential): A TensorFlow Keras Sequential model representing the discriminator.
-    :return:
-    """
-    model = tf.keras.models.Sequential()
-    # your code here
-
-    return model
-
-def build_generator():
-    """
-    The generator model takes a 100-dimensional noise vector as input and outputs a generated
-    image with a shape of (28, 28, 1).
-
-    Model Architecture:
-    1. Dense layer with 7 * 7 * 8 (392) neurons and no bias, input shape of (100,).
-    2. Batch normalization layer, default params
-    3. LeakyReLU activation function with default params
-    4. Reshape layer to convert the 1D array into a 3D feature map with a shape of (7, 7, 8).
-    5. Conv2DTranspose (deconvolution) layer with 8 filters, kernel size of (5, 5), strides of (1, 1)
-    6. Batch normalization layer.
-    7. LeakyReLU activation function with default params
-    8. Conv2DTranspose (deconvolution) layer with 16 filters, kernel size of (5, 5), strides of (2, 2)
-    9. Batch normalization layer.
-    10. LeakyReLU activation function with default params
-    11. Conv2DTranspose (deconvolution) layer with 1 filter, kernel size of (5, 5), strides of (2, 2), with tanh activation included
-
-    Note: For all Conv2DTranspose, use padding='same' and use_bias=False.
-
     Returns:
-        model (tf.keras.models.Sequential): A TensorFlow Keras Sequential model representing the generator.
+    model (tf.keras.models.Sequential): A TensorFlow Keras Sequential model representing the discriminator.
     """
     model = tf.keras.models.Sequential()
-    # your code here
+    model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(2, 2), padding='same', input_shape=(28, 28, 1)))
+    model.add(tf.keras.layers.LeakyReLU())
+    model.add(tf.keras.layers.Dropout(rate=0.3))
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(5, 5), strides=(2, 2), padding='same'))
+    model.add(tf.keras.layers.LeakyReLU())
+    model.add(tf.keras.layers.Dropout(rate=0.3))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(units=1))
 
     return model
+
+# def build_generator():
+#     """
+#     The generator model takes a 100-dimensional noise vector as input and outputs a generated
+#     image with a shape of (28, 28, 1).
+
+#     Model Architecture:
+#     1. Dense layer with 7 * 7 * 8 (392) neurons and no bias, input shape of (100,).
+#     2. Batch normalization layer, default params
+#     3. LeakyReLU activation function with default params
+#     4. Reshape layer to convert the 1D array into a 3D feature map with a shape of (7, 7, 8).
+#     5. Conv2DTranspose (deconvolution) layer with 8 filters, kernel size of (5, 5), strides of (1, 1)
+#     6. Batch normalization layer.
+#     7. LeakyReLU activation function with default params
+#     8. Conv2DTranspose (deconvolution) layer with 16 filters, kernel size of (5, 5), strides of (2, 2)
+#     9. Batch normalization layer.
+#     10. LeakyReLU activation function with default params
+#     11. Conv2DTranspose (deconvolution) layer with 1 filter, kernel size of (5, 5), strides of (2, 2), with tanh activation included
+
+#     Note: For all Conv2DTranspose, use padding='same' and use_bias=False.
+
+#     Returns:
+#         model (tf.keras.models.Sequential): A TensorFlow Keras Sequential model representing the generator.
+#     """
+#     model = tf.keras.models.Sequential()
+#     # your code here
+
+#     return model
 
 class DCGAN(tf.keras.Model):
     def __init__(self, discriminator, generator):
